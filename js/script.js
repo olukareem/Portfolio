f6cf963dcdf6de60d07cb3b5b80da1638afa1528
glolorun <63762681+glolorun@users.noreply.github.com>
2020-07-28 09:48:48 -0400

    need to do nav

--- js/script.js
+++ js/script.js
@@ -1,71 +1,104 @@
 $(document).ready(function () {
   $("#slides").superslides({
-        animation: "fade",
-        play: 5000,
-        pagination: false,
+    animation: "fade",
+    play: 5000,
+    pagination: false,
   });
   var typed = new Typed(".typed", {
-        strings: ["Software Engineer", "Full Stack Web Developer"],
-        typeSpeed: 100,
-        loop: true,
-        startDelay: 0,
-        showCursor: false,
+    strings: ["Software Engineer", "Full Stack Web Developer"],
+    typeSpeed: 100,
+    loop: true,
+    startDelay: 0,
+    showCursor: false,
   });
-  var typed2 = new Typed(".typed2", {
-        strings: ["javascript", "ruby"],
-        typeSpeed: 0,
-        loop: true,
-        startDelay: 1000,
-        showCursor: false,
-        smartBackspace: true,
-        fadeOutDelay: 500,
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
   });
-  $('.owl-carousel').owlCarousel({
-      loop: true,
-      items: 4,
-    responsive:{
-        0:{
-            items:1
+  $(".chart").easyPieChart({
+    easing: "easeInOut",
+    barColor: "#fff",
+    trackColor: false,
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onStep: function (from, to, percent) {
+      $(this.el).find(".percent").text(Math.round(percent));
+    },
+  });
+
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
         },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        938:{
-            items:4
-        }
+      });
     }
-})
-  $('.chart').easyPieChart({
-      easing: 'easeInOut',
-      barColor: '#fff',
-      trackColor: false,
-      scaleColor: false,
-      lineWidth: 4,
-      size: 152,
-      onStep: function (from, to, percent) {
-          $(this.el).find('.percent').text(Math.round(percent));
-      }
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+
+        element.countup(endVal);
+      });
+      countUpFinished = true;
+    }
   });
     
-    var skillsTopOffset = $(".skillsSection").offset().top;
-    
-    $(window).scroll(function () {
-        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-            $('.chart').easyPieChart({
-                easing: 'easeInOut',
-                barColor: '#fff',
-                trackColor: false,
-                scaleColor: false,
-                lineWidth: 4,
-                size: 152,
-                onStep: function (from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent));
-                }
-            });
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
         }
+    });
+    $("#filters a").click(function () {
+        $("#filters.current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
 
     });
+    
 });
